8569835e54e09c45a33c26f73c79adf1a4db8bff
Rick Kauffman <rick@rickkauffman.com>
2022-10-13 19:10:16 -0700

    alpha-4

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name = 'pymellon',
     packages = ['pymellon'],
-    version = '0.2.1',
+    version = '0.2.12,
     description = 'A python binding to work with the Mellanox API',
     author = 'Rick Kauffman',
     author_email = 'rick@rickkauffman.com',
     url = 'https://github.com/xod442/pymellon',
-    download_url = 'https://github.com/xod442/pymellon/archive/refs/tags/v.0.2.1.tar.gz',
+    download_url = 'https://github.com/xod442/pymellon/archive/refs/tags/v.0.2.2.tar.gz',
     keywords = ['mellanox', 'api', 'python'],
     install_requires=[
           'requests',
